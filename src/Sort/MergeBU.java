911f3751f87e372c82281560d51fef2a5de26a9f
igaozp <igaozp@hotmail.com>
2017-07-02 15:52:09 +0800

    自底向上的归并排序

---
+++ src/Sort/MergeBU.java
@@ -0,0 +1,40 @@
+package Sort;
+
+public class MergeBU {
+    private static Comparable[] aux;
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz = sz + sz) {
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            }
+        }
+    }
+
+    public static void merge(Comparable[] a, int lo, int mid, int hi) {
+        int i = lo;
+        int j = mid + 1;
+
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (less(aux[j], aux[i])) {
+                a[k] = aux[j++];
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+}
