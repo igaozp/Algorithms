c80d462650d7fde401acfaa1a33a563e41d4f4ed
igaozp <igaozp@hotmail.com>
2017-07-10 13:54:51 +0800

    添加基于拉链法的哈希表实现

---
+++ src/Find/SeparateChainingHashST.java
@@ -0,0 +1,44 @@
+package Find;
+
+import Base.Queue;
+
+public class SeparateChainingHashST<Key, Value> {
+    private int N;
+    private int M;
+    private SequentialSearchST<Key, Value>[] st;
+
+    public SeparateChainingHashST() {
+        this(997);
+    }
+
+    public SeparateChainingHashST(int M) {
+        this.M = M;
+        st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+        for (int i = 0; i < M; i++) {
+            st[i] = new SequentialSearchST();
+        }
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public Value get(Key key) {
+        return (Value) st[hash(key)].get(key);
+    }
+
+    public void put(Key key, Value val) {
+        st[hash(key)].put(key, val);
+    }
+
+    public Iterable<Key> keys() {
+        Queue<Key> q = new Queue<>();
+        for (int i = 0; i < M; i++) {
+            Queue<Key> temp = (Queue<Key>) st[i].keys();
+            for (Key key : temp) {
+                q.enqueue(key);
+            }
+        }
+        return q;
+    }
+}
