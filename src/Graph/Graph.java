6232e1a6f56bb16399824d9cf66edd94b543288a
igaozp <igaozp@hotmail.com>
2017-07-11 20:53:49 +0800

    添加无向图的算法实现

---
+++ src/Graph/Graph.java
@@ -0,0 +1,93 @@
+package Graph;
+
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.In;
+
+public class Graph {
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = E;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public Graph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            adj[v] = new Bag<Integer>();
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public static int degree(Graph G, int v) {
+        int degree = 0;
+        for (int w : G.adj(v)) {
+            degree++;
+        }
+        return degree;
+    }
+
+    public static int maxDegree(Graph G) {
+        int max = 0;
+        for (int v = 0; v < G.V(); v++) {
+            if (degree(G, v) > max) {
+                max = degree(G, v);
+            }
+        }
+        return max;
+    }
+
+    public static double avgDegree(Graph G) {
+        return 2.0 * G.E() / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G) {
+        int count = 0;
+        for (int v = 0; v < G.V(); v++) {
+            for (int w : G.adj(v)) {
+                if (v == w) {
+                    count++;
+                }
+            }
+        }
+        return count / 2;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder(V + " vertices, " + E + " edges\n");
+        for (int v = 0; v < V; v++) {
+            s.append(v).append(": ");
+            for (int w : this.adj(v)) {
+                s.append(w).append(" ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+}
