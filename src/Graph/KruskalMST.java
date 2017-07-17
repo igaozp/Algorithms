f70058a20e6df53a857a16fa3b254774ce75ac94
igaozp <igaozp@hotmail.com>
2017-07-17 14:22:53 +0800

    添加最小生成树的 Kruskal 算法的实现

---
+++ src/Graph/KruskalMST.java
@@ -0,0 +1,41 @@
+package Graph;
+
+import Base.Queue;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.UF;
+
+public class KruskalMST {
+    private Queue<Edge> mst;
+
+    public KruskalMST(EdgeWeightedGraph G) {
+        mst = new Queue<Edge>();
+        MinPQ<Edge> pq = new MinPQ<Edge>();
+        for (Edge e : G.edges()) {
+            pq.insert(e);
+        }
+        UF uf = new UF(G.V());
+
+        while (!pq.isEmpty() && mst.size() < G.V() - 1) {
+            Edge e = pq.delMin();
+            int v = e.either();
+            int w = e.other(v);
+            if (uf.connected(v, w)) {
+                continue;
+            }
+            uf.union(v, w);
+            mst.enqueue(e);
+        }
+    }
+
+    public Iterable<Edge> edges() {
+        return mst;
+    }
+
+    public double weight() {
+        double weight = 0;
+        for (Edge e : this.edges()) {
+            weight += e.weight();
+        }
+        return weight;
+    }
+}
