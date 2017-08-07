48a15066a4cdf2ac71902dd65c6eaa4efae7d833
KuoPing Kao <igaozp@hotmail.com>
2017-08-07 16:11:02 +0800

    为 MergeBU.java 添加 Javadoc 注释

--- src/Sort/MergeBU.java
+++ src/Sort/MergeBU.java
@@ -1,26 +1,54 @@
 package Sort;
 
+/**
+ * 自底向上的归并排序
+ *
+ * 自底向上的归并排序通过把多个小的数组归并排序，逐渐完成整个数组的排序
+ *
+ * @author igaozp
+ * @since 2017-7-2
+ * @version 1.0
+ */
 public class MergeBU {
+    /**
+     * 用于暂存的数组
+     */
     private static Comparable[] aux;
 
+    /**
+     * 归并排序
+     *
+     * @param a 需要排序的数组
+     */
     public static void sort(Comparable[] a) {
         int N = a.length;
         aux = new Comparable[N];
         for (int sz = 1; sz < N; sz = sz + sz) {
             for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                // 归并数组
                 merge(a, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
             }
         }
     }
 
+    /**
+     * 将两个有序数组归并为一个有序的数组
+     *
+     * @param a 需要归并操作的数组
+     * @param lo 归并数组的开始下标
+     * @param mid 归并数组的中间下标
+     * @param hi 归并数组的结束下标
+     */
     public static void merge(Comparable[] a, int lo, int mid, int hi) {
         int i = lo;
         int j = mid + 1;
 
+        // 将 a[lo..hi] 复制到 aux[lo..hi]
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
         }
 
+        // 归并回到 a[lo..hi]
         for (int k = lo; k <= hi; k++) {
             if (i > mid) {
                 a[k] = aux[j++];
@@ -34,6 +62,14 @@ public class MergeBU {
         }
     }
 
+    /**
+     * 比较两个参数的大小
+     *
+     * @param v 比较的第一个参数
+     * @param w 比较的第二个参数
+     * @return {@code true} 第一个参数比第二个小
+     *         {@code false} 第一个参数比第二个大
+     */
     private static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
