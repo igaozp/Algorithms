03b687eeb190142a870bb443e7295720b403e575
KuoPing Kao <igaozp@hotmail.com>
2017-08-15 16:30:01 +0800

    为 BST.java 添加 Javadoc 注释

--- src/Find/BST.java
+++ src/Find/BST.java
@@ -2,15 +2,38 @@ package Find;
 
 import Base.Queue;
 
+/**
+ * 二叉查找树
+ *
+ * @author igaozp
+ * @since 2017-07-05
+ * @version 1.0
+ *
+ * @param <Key> 泛型类型
+ * @param <Value> 泛型类型
+ */
 public class BST<Key extends Comparable<Key>, Value> {
+    /**
+     * 二叉树的根节点
+     */
     private Node root;
 
+    /**
+     * 内部定义的节点类
+     */
     private class Node {
         private Key key;
         private Value val;
         private Node left, right;
         private int N;
 
+        /**
+         * 构造方法
+         *
+         * @param key 节点的键
+         * @param val 节点的值
+         * @param N 子节点的数量
+         */
         public Node(Key key, Value val, int N) {
             this.key = key;
             this.val = val;
@@ -18,10 +41,21 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    /**
+     * 获取二叉树的节点数量
+     *
+     * @return 节点数量
+     */
     public int size() {
         return size(root);
     }
 
+    /**
+     * 获取指定节点的子节点数量
+     *
+     * @param x 指定节点
+     * @return 节点数量
+     */
     private int size(Node x) {
         if (x == null) {
             return 0;
@@ -30,10 +64,23 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    /**
+     * 获取指定的键的值
+     *
+     * @param key 指定的键
+     * @return 指定键的值
+     */
     public Value get(Key key) {
         return get(root, key);
     }
 
+    /**
+     * 在指定节点下获取指定键的值
+     *
+     * @param x 指定的节点
+     * @param key 指定的键
+     * @return 指定键的值
+     */
     private Value get(Node x, Key key) {
         if (x == null) {
             return null;
@@ -49,10 +96,24 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    /**
+     * 向二叉树中添加新的节点
+     *
+     * @param key 节点的键
+     * @param val 节点的值
+     */
     public void put(Key key, Value val) {
         root = put(root, key, val);
     }
 
+    /**
+     * 在指定节点下添加新的节点
+     *
+     * @param x 指定的节点
+     * @param key 新节点的键
+     * @param val 新节点的值
+     * @return 添加节点的指定节点
+     */
     public Node put(Node x, Key key, Value val) {
         if (x == null) {
             return new Node(key, val, 1);
@@ -71,10 +132,21 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
+    /**
+     * 获取二叉函数中最小元素的键
+     *
+     * @return 最小元素的键
+     */
     public Key min() {
         return min(root).key;
     }
 
+    /**
+     * 在指定节点下获取最小元素的节点
+     *
+     * @param x 指定的节点
+     * @return 最小元素的节点
+     */
     private Node min(Node x) {
         if (x.left == null) {
             return x;
@@ -82,10 +154,21 @@ public class BST<Key extends Comparable<Key>, Value> {
         return min(x.left);
     }
 
+    /**
+     * 获取二叉函数中最大元素的键
+     *
+     * @return 最大元素的键
+     */
     public Key max() {
         return max(root).key;
     }
 
+    /**
+     * 在指定节点大获取最小元素的节点
+     *
+     * @param x 指定的节点
+     * @return 最大元素的节点
+     */
     private Node max(Node x) {
         if (x.right == null) {
             return x;
@@ -93,6 +176,12 @@ public class BST<Key extends Comparable<Key>, Value> {
         return max(x.right);
     }
 
+    /**
+     * 在二叉树中获取最大的不大于指定键的节点的键
+     *
+     * @param key 指定的键
+     * @return 不大于指定键的键
+     */
     public Key floor(Key key) {
         Node x = floor(root, key);
         if (x == null) {
@@ -101,6 +190,13 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x.key;
     }
 
+    /**
+     * 在指定的节点下获取最大的不大于指定键的节点
+     *
+     * @param x 指定节点
+     * @param key 指定键
+     * @return 最大的节点
+     */
     private Node floor(Node x, Key key) {
         if (x == null) {
             return null;
@@ -121,6 +217,12 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    /**
+     * 在二叉树中获取最小的不小于指定键的节点的键
+     *
+     * @param key 指定的键
+     * @return 不小于指定键的键
+     */
     public Key ceiling(Key key) {
         Node x = ceiling(root, key);
         if (x == null) {
@@ -129,6 +231,13 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x.key;
     }
 
+    /**
+     * 在指定的节点下获取最小的不小于指定键的节点
+     *
+     * @param x 指定节点
+     * @param key 指定键
+     * @return 最小的节点
+     */
     private Node ceiling(Node x, Key key) {
         if (x == null) {
             return null;
@@ -149,10 +258,23 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    /**
+     * 在二叉树中查找指定大小的子树
+     *
+     * @param k 子树的大小
+     * @return 子树的键
+     */
     public Key select(int k) {
         return select(root, k).key;
     }
 
+    /**
+     * 在指定节点下根据子树大小查找相应的节点
+     *
+     * @param x 指定的节点
+     * @param k 子树的大小
+     * @return 查找到的节点
+     */
     private Node select(Node x, int k) {
         if (x == null) {
             return null;
@@ -167,10 +289,23 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    /**
+     * 根据键查找相应节点子树的节点数量
+     *
+     * @param key 查找的键
+     * @return 节点数量
+     */
     public int rank(Key key) {
         return rank(key, root);
     }
 
+    /**
+     * 在指定节点下通过键查找节点的子节点数量
+     *
+     * @param key 指定的键
+     * @param x 指定的节点
+     * @return 节点数量
+     */
     private int rank(Key key, Node x) {
         if (x == null) {
             return 0;
@@ -185,10 +320,19 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
     }
 
+    /**
+     * 删除二叉树中最小的节点
+     */
     public void deleteMin() {
         root = deleteMin(root);
     }
 
+    /**
+     * 在指定的节点下删除最小的节点
+     *
+     * @param x 指定的节点
+     * @return 删除后的指定节点
+     */
     private Node deleteMin(Node x) {
         if (x.left == null) {
             return x.right;
@@ -198,10 +342,22 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
+    /**
+     * 根据键删除相应的节点
+     *
+     * @param key 需要删除的节点的键
+     */
     public void delete(Key key) {
         root = delete(root, key);
     }
 
+    /**
+     * 在指定节点下根据键删除节点
+     *
+     * @param x 指定的节点
+     * @param key 指定的键
+     * @return 删除后的指定节点
+     */
     private Node delete(Node x, Key key) {
         if (x == null) {
             return null;
@@ -227,16 +383,36 @@ public class BST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
+    /**
+     * 获取二叉树中所有的键
+     *
+     * @return
+     */
     public Iterable<Key> keys() {
         return keys(min(), max());
     }
 
+    /**
+     * 根据键的范围获取范围内的键
+     *
+     * @param lo 开始范围
+     * @param hi 结束范围
+     * @return 包含键的队列
+     */
     public Iterable<Key> keys(Key lo, Key hi) {
         Queue<Key> queue = new Queue<Key>();
         keys(root, queue, lo, hi);
         return queue;
     }
 
+    /**
+     * 在指定节点和范围下获取所有键的集合
+     *
+     * @param x 指定的节点
+     * @param queue 存储键的队列
+     * @param lo 开始范围
+     * @param hi 结束范围
+     */
     private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
         if (x == null) {
             return;
