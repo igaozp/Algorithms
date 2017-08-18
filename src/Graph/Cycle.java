a22a24ed0dd7e192f5cfd9fdfd76cceb087b3749
KuoPing Kao <igaozp@hotmail.com>
2017-08-18 16:45:07 +0800

    为 Cycle.java 添加 Javadoc 注释

--- src/Graph/Cycle.java
+++ src/Graph/Cycle.java
@@ -1,9 +1,27 @@
 package Graph;
 
+/**
+ * 检测无环图
+ *
+ * @author igaozp
+ * @since 2017-07-13
+ * @version 1.0
+ */
 public class Cycle {
+    /**
+     * 存储每个顶点的访问情况
+     */
     private boolean[] marked;
+    /**
+     * 是否有环
+     */
     private boolean hasCycle;
 
+    /**
+     * 构造函数
+     *
+     * @param G 初始化的图
+     */
     public Cycle(Graph G) {
         marked = new boolean[G.V()];
         for (int s = 0; s < G.V(); s++) {
@@ -13,6 +31,13 @@ public class Cycle {
         }
     }
 
+    /**
+     * 深度优先搜索
+     *
+     * @param G 搜索的图
+     * @param v 搜索的起点
+     * @param u 对比顶点
+     */
     private void dfs(Graph G, int v, int u) {
         marked[v] = true;
         for (int w : G.adj(v)) {
@@ -24,6 +49,12 @@ public class Cycle {
         }
     }
 
+    /**
+     * 检查图是否有环
+     *
+     * @return {@code true} 有环
+     *         {@code false} 无环
+     */
     public boolean hasCycle() {
         return hasCycle;
     }
