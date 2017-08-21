c9db264d23c66ea28b4c2060723d2bee2f5a384a
KuoPing Kao <igaozp@hotmail.com>
2017-08-21 13:12:24 +0800

    为 DirectedEdge.java 添加 Javadoc 注释

--- src/Graph/DirectedEdge.java
+++ src/Graph/DirectedEdge.java
@@ -1,24 +1,62 @@
 package Graph;
 
+/**
+ * 加权有向图的边
+ *
+ * @author igaozp
+ * @since 2017-07-18
+ * @version 1.0
+ */
 public class DirectedEdge {
+    /**
+     * 边的起点
+     */
     private final int v;
+    /**
+     * 边的终点
+     */
     private final int w;
+    /**
+     * 边的权重
+     */
     private final double weight;
 
+    /**
+     * 构造方法
+     *
+     * @param v 边的起点
+     * @param w 边的终点
+     * @param weight 边的权重
+     */
     public DirectedEdge(int v, int w, double weight) {
         this.v = v;
         this.w = w;
         this.weight = weight;
     }
 
+    /**
+     * 获取边的权重
+     *
+     * @return 边的权重
+     */
     public double weight() {
         return weight;
     }
 
+    /**
+     * 获取边的起点
+     *
+     * @return 边的起点
+     */
     public int from() {
         return v;
     }
 
+    /**
+     * 获取边的终点
+     *
+     * @return 边的终点
+     */
     public int to() {
         return w;
     }
