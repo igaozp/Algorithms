6371ef0eab9d439d52e6e14aff8ca63b9ebf4e98
KuoPing Kao <igaozp@hotmail.com>
2017-08-21 13:02:41 +0800

    为 KruskalMST.java 添加 Javadoc 注释

--- src/Graph/KruskalMST.java
+++ src/Graph/KruskalMST.java
@@ -4,9 +4,24 @@ import Base.Queue;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.UF;
 
+/**
+ * 最小生成树的 Kruskal 算法
+ *
+ * @author igaozp
+ * @since 2017-07-17
+ * @version 1.0
+ */
 public class KruskalMST {
+    /**
+     * 最小生成树
+     */
     private Queue<Edge> mst;
 
+    /**
+     * 构造方法
+     *
+     * @param G 加权无向图
+     */
     public KruskalMST(EdgeWeightedGraph G) {
         mst = new Queue<Edge>();
         MinPQ<Edge> pq = new MinPQ<Edge>();
@@ -27,10 +42,20 @@ public class KruskalMST {
         }
     }
 
+    /**
+     * 获取最小生成树
+     *
+     * @return 最小生成树
+     */
     public Iterable<Edge> edges() {
         return mst;
     }
 
+    /**
+     * 获取最小生成树的权重
+     *
+     * @return 最下生成树的权重
+     */
     public double weight() {
         double weight = 0;
         for (Edge e : this.edges()) {
