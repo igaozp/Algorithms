f0ea35fe4bb7b0ff74330c4c615c059bdc3508b2
KuoPing Kao <igaozp@hotmail.com>
2017-08-21 14:56:38 +0800

    为 BellmanFordSP.java 添加 Javadoc 注释

--- src/Graph/BellmanFordSP.java
+++ src/Graph/BellmanFordSP.java
@@ -4,14 +4,45 @@ import edu.princeton.cs.algs4.*;
 import edu.princeton.cs.algs4.DirectedEdge;
 import edu.princeton.cs.algs4.EdgeWeightedDigraph;
 
+/**
+ * 基于队列的 Bellman-Ford 算法
+ *
+ * @author igaozp
+ * @since 2017-07-19
+ * @version 1.0
+ */
 public class BellmanFordSP {
+    /**
+     * 到某个顶点的路径长度
+     */
     private double[] distTo;
+    /**
+     * 到某个顶点的最后一条边
+     */
     private DirectedEdge[] edgeTo;
+    /**
+     * 该顶点是否在队列中
+     */
     private boolean[] onQ;
+    /**
+     * 正在被放松的顶点
+     */
     private Queue<Integer> queue;
+    /**
+     * relax() 的调用次数
+     */
     private int cost;
+    /**
+     * 是否有负权重环
+     */
     private Iterable<DirectedEdge> cycle;
 
+    /**
+     * 构造方法
+     *
+     * @param G 加权有向图
+     * @param s 起始顶点
+     */
     public BellmanFordSP(EdgeWeightedDigraph G, int s) {
         distTo = new double[G.V()];
         edgeTo = new DirectedEdge[G.V()];
@@ -30,6 +61,12 @@ public class BellmanFordSP {
         }
     }
 
+    /**
+     * 顶点的松弛
+     *
+     * @param G 加权有向图
+     * @param v 指定的顶点
+     */
     private void relax(EdgeWeightedDigraph G, int v) {
         for (DirectedEdge e : G.adj(v)) {
             int w = e.to();
@@ -47,14 +84,33 @@ public class BellmanFordSP {
         }
     }
 
+    /**
+     * 到指定顶点的路径长度
+     *
+     * @param v 指定的顶点
+     * @return 路径长度
+     */
     public double distTo(int v) {
         return distTo[v];
     }
 
+    /**
+     * 检查到指定顶点是否有路径
+     *
+     * @param v 指定的顶点
+     * @return {@code true} 有路径
+     *         {@code false} 没有路径
+     */
     public boolean hasPathTo(int v) {
         return distTo[v] < Double.POSITIVE_INFINITY;
     }
 
+    /**
+     * 获取到指定顶点的路径
+     *
+     * @param v 指定的顶点
+     * @return 路径
+     */
     public Iterable<DirectedEdge> pathTo(int v) {
         if (!hasPathTo(v)) {
             return null;
@@ -66,6 +122,9 @@ public class BellmanFordSP {
         return path;
     }
 
+    /**
+     * 查找负权重的环
+     */
     private void findNegativeCycle() {
         int V = edgeTo.length;
         EdgeWeightedDigraph spt;
@@ -79,10 +138,21 @@ public class BellmanFordSP {
         cycle = cf.cycle();
     }
 
+    /**
+     * 检查是否有负权重的环
+     *
+     * @return {@code true} 有负权重的环
+     *         {@code false} 没有负权重的环
+     */
     public boolean hasNegativeCycle() {
         return cycle != null;
     }
 
+    /**
+     * 获取负权重的环
+     *
+     * @return 负权重的环
+     */
     public Iterable<DirectedEdge> negativeCycle() {
         return cycle;
     }
