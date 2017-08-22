c007592c385f71225e9b30c0a8f0e31d85641d6c
KuoPing Kao <igaozp@hotmail.com>
2017-08-22 10:56:17 +0800

    为 LSD.java 添加 Javadoc 注释

--- src/String/LSD.java
+++ src/String/LSD.java
@@ -1,24 +1,44 @@
 package String;
 
+/**
+ * 低位优先的字符排序算法
+ *
+ * @author igaozp
+ * @since 2017-07-21
+ * @version 1.0
+ */
 public class LSD {
+    /**
+     * 字符串排序
+     *
+     * 假设每一个字符串的长度相等，从每一个字符串的低位到高位进行排序，
+     * 最后完成字符串的排序
+     *
+     * @param a 需要排序的字符串数组
+     * @param W 字符串长度
+     */
     public static void sort(String[] a, int W) {
         int N = a.length;
         int R = 256;
         String[] aux = new String[N];
+
         for (int d = W - 1; d >= 0; d--) {
+            // 计算出现的频率
             int[] count = new int[R + 1];
-            for (int i = 0; i < N; i++) {
-                count[a[i].charAt(d) + 1]++;
+            for (String s : a) {
+                count[s.charAt(d) + 1]++;
             }
-            for (int r = 0; r < R; r++) {
-                count[r + 1] += count[r];
+
+            // 将频率转换为索引
+            for (int r = 0; r < R; r++) count[r + 1] += count[r];
+
+            // 将元素分类
+            for (String s : a) {
+                aux[count[s.charAt(d)]++] = s;
             }
-            for (int i = 0; i < N; i++) {
-                aux[count[a[i].charAt(d)]++] = a[i];
-            }
-            for (int i = 0; i < N; i++) {
-                a[i] = aux[i];
-            }
+
+            // 回写
+            System.arraycopy(aux, 0, a, 0, N);
         }
     }
 }
