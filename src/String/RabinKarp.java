2c4b93b0c0c0d47ff651faaeafe646c6f6475841
KuoPing Kao <igaozp@hotmail.com>
2017-08-23 14:59:02 +0800

    为 RabinKarp.java 添加 Javadoc 注释

--- src/String/RabinKarp.java
+++ src/String/RabinKarp.java
@@ -3,14 +3,44 @@ package String;
 import java.math.BigInteger;
 import java.util.Random;
 
+/**
+ * Rabin-Karp 指纹字符串查找算法
+ *
+ * @author igaozp
+ * @since 2017-07-29
+ * @version 1.0
+ */
 public class RabinKarp {
+    /**
+     * 用于匹配的模式字符串
+     */
     private String pat;
+    /**
+     * 模式字符串的散列值
+     */
     private long patHash;
+    /**
+     * 模式字符串的长度
+     */
     private int M;
+    /**
+     * 一个很大的素数
+     */
     private int Q;
+    /**
+     * 字母表的大小
+     */
     private int R = 256;
+    /**
+     * R ^ (M - 1) % Q
+     */
     private long RM;
 
+    /**
+     * 构造方法
+     *
+     * @param pat 模式字符串
+     */
     public RabinKarp(String pat) {
         this.pat = pat;
         this.M = pat.length();
@@ -22,16 +52,35 @@ public class RabinKarp {
         patHash = hash(pat, M);
     }
 
+    /**
+     * 生成一个长的随机素数
+     *
+     * @return 素数
+     */
     private static long longRandomPrime()
     {
         BigInteger prime = BigInteger.probablePrime(31, new Random());
         return prime.longValue();
     }
 
+    /**
+     * 检查模式是否匹配
+     *
+     * @param i 下标
+     * @return {@code true} 匹配
+     *         {@code false} 不匹配
+     */
     public boolean check(int i) {
         return true;
     }
 
+    /**
+     * 生成哈希值
+     *
+     * @param key 键
+     * @param M 用于生成哈希值的键的位数
+     * @return 哈希值
+     */
     private long hash(String key, int M) {
         long h = 0;
         for (int j = 0; j < M; j++) {
@@ -40,6 +89,12 @@ public class RabinKarp {
         return h;
     }
 
+    /**
+     * 字符串搜素
+     *
+     * @param txt 用作搜索的文本
+     * @return 搜索到的字符串下标
+     */
     private int search(String txt) {
         int N = txt.length();
         long txtHash = hash(txt, M);
