8ab35bb257b59082d87bfdf6ed2e92aaf5e7a704
KuoPing Kao <igaozp@hotmail.com>
2017-08-23 14:47:33 +0800

    为 BoyerMoore.java 添加 Javadoc 注释

--- src/String/BoyerMoore.java
+++ src/String/BoyerMoore.java
@@ -2,23 +2,49 @@ package String;
 
 import edu.princeton.cs.algs4.StdOut;
 
+/**
+ * Boyer-Moore 字符串匹配算法
+ *
+ * @author igaozp
+ * @since 2017-07-27
+ * @version 1.0
+ */
 public class BoyerMoore {
+    /**
+     * 记录每个字符在模式中出现的最靠右的地方
+     */
     private int[] right;
+    /**
+     * 匹配的字符串（模式）
+     */
     private String pat;
 
+    /**
+     * 构造方法
+     *
+     * @param pat 匹配的模式
+     */
     BoyerMoore(String pat) {
         this.pat = pat;
         int M = pat.length();
         int R = 256;
         right = new int[R];
+
         for (int c = 0; c < R; c++) {
             right[c] = -1;
         }
+
         for (int j = 0; j < M; j++) {
             right[pat.charAt(j)] = j;
         }
     }
 
+    /**
+     * 字符串查找
+     *
+     * @param txt 用于查找字符串的文本
+     * @return 查找到的字符串下标
+     */
     public int search(String txt) {
         int N = txt.length();
         int M = pat.length();
@@ -41,13 +67,20 @@ public class BoyerMoore {
         return N;
     }
 
+    /**
+     * 测试
+     *
+     * @param args 命令行参数
+     */
     public static void main(String[] args) {
         String pat = args[0];
         String txt = args[1];
         BoyerMoore boyerMoore = new BoyerMoore(pat);
+
         StdOut.println("text:   " + txt);
         int offset = boyerMoore.search(txt);
         StdOut.print("pattern: ");
+
         for (int i = 0; i < offset; i++) {
             StdOut.print(" ");
         }
