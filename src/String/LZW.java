692fa690dad7db7ab8207938f2740c734780be2b
KuoPing Kao <igaozp@hotmail.com>
2017-08-24 20:01:59 +0800

    为 LZW.java 添加 Javadoc 注释

--- src/String/LZW.java
+++ src/String/LZW.java
@@ -4,11 +4,30 @@ import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 import edu.princeton.cs.algs4.TST;
 
+/**
+ * LZW 压缩
+ *
+ * @author igaozp
+ * @since 1.0
+ * @version 1.0
+ */
 public class LZW {
+    /**
+     * 输入的字符数
+     */
     private static final int R = 256;
+    /**
+     * 编码总数
+     */
     private static final int L = 4096;
+    /**
+     * 编码宽度
+     */
     private static final int W = 12;
 
+    /**
+     * 压缩
+     */
     public static void compress() {
         String input = BinaryStdIn.readString();
         TST<Integer> st = new TST<Integer>();
@@ -16,6 +35,7 @@ public class LZW {
         for (int i = 0; i < R; i++) {
             st.put("" + (char) i, i);
         }
+
         int code = R + 1;
 
         while (input.length() > 0) {
@@ -28,13 +48,18 @@ public class LZW {
             }
             input = input.substring(t);
         }
+
         BinaryStdOut.write(R, W);
         BinaryStdOut.close();
     }
 
+    /**
+     * 解码
+     */
     public static void expand() {
         String[] st = new String[L];
         int i;
+
         for (i = 0; i < R; i++) {
             st[i] = "" + (char) i;
         }
@@ -42,16 +67,19 @@ public class LZW {
 
         int codeword = BinaryStdIn.readInt(W);
         String val = st[codeword];
+
         while (true) {
             BinaryStdOut.write(val);
             codeword = BinaryStdIn.readInt(W);
             if (codeword == R) {
                 break;
             }
+
             String s = st[codeword];
             if (i == codeword) {
                 s = val + val.charAt(0);
             }
+
             if (i < L) {
                 st[i++] = val + s.charAt(0);
             }
