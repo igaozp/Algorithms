24d6b6838feea84672700d6663b4d0d069a39ea0
KuoPing Kao <igaozp@hotmail.com>
2017-09-08 18:36:26 +0800

    添加 KTBreadthFirstSearch.kt

---
+++ Kotlin/src/Graph/KTBreadthFirstSearch.kt
@@ -0,0 +1,75 @@
+package Graph
+
+import Base.Queue
+
+/**
+ * 图的广度优先搜索
+ *
+ * @author igaozp
+ * @since 2017-09-08
+ * @version 1.0
+ */
+class KTBreadthFirstSearch(G: Graph, s: Int) {
+    /**
+     * 存储图顶点的访问情况
+     */
+    private var marked: Array<Boolean>? = null
+    /**
+     * 存储顶点的访问次数
+     */
+    private var count: Int = 0
+
+    /**
+     * 构造方法
+     */
+    init {
+        marked = Array(G.V())
+        bfs(G, s)
+    }
+
+    /**
+     * 生成函数的辅助函数
+     */
+    private fun <T> Array(size: Int): Array<T> = Array(size)
+
+    /**
+     * 广度优先搜索
+     *
+     * @param G 需要搜索的图
+     * @param s 搜索的起点
+     */
+    private fun bfs(G: Graph, s: Int) {
+        // 将访问过的元素加入队列
+        val queue = Queue<Int>()
+        marked!![s] = true
+        queue.enqueue(s)
+
+        // 依次访问以出队的元素为顶点的元素
+        while (!queue.isEmpty) {
+            val v = queue.dequeue()
+            for (w in G.adj(v)) {
+                // 没有访问则入队
+                if (!marked!![w]) {
+                    marked!![w] = true
+                    queue.enqueue(w)
+                }
+            }
+        }
+    }
+
+    /**
+     * 检查指定的顶点是否访问过
+     *
+     * @param w 指定的顶点
+     * @return `true` 访问过
+     *         `false` 未访问过
+     */
+    fun marked(w: Int): Boolean = marked!![w]
+
+    /**
+     * 获取图的访问次数
+     *
+     * @return 访问次数
+     */
+    fun count(): Int = count
+}
