e1398cce9fd85baaf22a608683bdbcc747a3287c
KuoPing Kao <igaozp@hotmail.com>
2017-09-13 19:15:26 +0800

    添加 KTLSD.kt

---
+++ Kotlin/src/String/KTLSD.kt
@@ -0,0 +1,51 @@
+package String
+
+/**
+ * 低位优先的字符排序算法
+ *
+ * @author igaozp
+ * @since 2017-09-13
+ * @version 1.0
+ */
+class KTLSD {
+    /**
+     * 字符串排序
+     *
+     * 假设每一个字符串的长度相等，从每一个字符串的低位到高位进行排序，
+     * 最后完成字符串的排序
+     *
+     * @param a 需要排序的字符串数组
+     * @param W 字符串长度
+     */
+    fun sort(a: Array<String>, W: Int) {
+        val N = a.size
+        val R = 256
+        val aux = Array<String>(N)
+
+        for (d in (W - 1) downTo 0) {
+            // 计算出现的频率
+            val count = Array<Int>(R + 1)
+            for (s in a) {
+                count[s[d].toInt() + 1]++
+            }
+
+            // 将频率转换为索引
+            for (r in 0 until R) {
+                count[r + 1] += count[r]
+            }
+
+            // 将元素分类
+            for (s in a) {
+                aux[count[s[d].toInt() + 1]] = s
+            }
+
+            // 回写
+            System.arraycopy(aux, 0, a, 0, N)
+        }
+    }
+
+    /**
+     * 生成数组的辅助方法
+     */
+    private fun <T> Array(size: Int): Array<T> = Array(size)
+}
