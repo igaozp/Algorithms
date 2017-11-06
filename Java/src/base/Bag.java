be749cd1e83550895c87ec106cb795dab430e0f7
KuoPing Kao <igaozp@hotmail.com>
2017-11-06 20:36:25 +0800

    [Fix] 修复一些问题

--- Java/src/base/Bag.java
+++ Java/src/base/Bag.java
@@ -1,25 +1,23 @@
-package Base;
+package base;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Iterator;
 
 /**
- * Stack 链表的实现
- * Stack 是一种基于 LIFO 策略的集合类型
+ * Bag 的链表实现
+ * Bag 是一种不支持删除其中元素的集合数据类型，可以用来收集和遍历元素
  *
  * @author igaozp
+ * @version 1.2
  * @since 2017-6-30
- * @version 1.1
  * @param <Item> 泛型类型
  */
-public class Stack<Item> implements Iterable<Item> {
+public class Bag<Item> implements Iterable<Item> {
     /**
-     * 栈的顶部节点
+     * 链表的首节点
      */
     private Node first;
-    /**
-     * 栈的节点元素数量
-     */
-    private int N;
 
     /**
      * 内部的链表节点类
@@ -30,52 +28,16 @@ public class Stack<Item> implements Iterable<Item> {
     }
 
     /**
-     * 检查栈是否为空
+     * 添加元素
      *
-     * @return {@code true} 栈为空
-     *         {@code false} 栈不为空
+     * @param item 添加的元素
      */
-    public boolean isEmpty() {
-        return first == null;
-    }
-
-    /**
-     * 检查栈的节点元素数量
-     *
-     * @return 节点元素数量
-     */
-    public int size() {
-        return N;
-    }
-
-    /**
-     * 元素压入栈
-     *
-     * @param item 入栈的元素
-     */
-    public void push(Item item) {
-        // 使用头插法将元素入栈
+    public void add(Item item) {
+        // 使用头插法插入新的元素
         Node oldFirst = this.first;
         this.first = new Node();
         this.first.item = item;
         this.first.next = oldFirst;
-        // 更新节点数量
-        N++;
-    }
-
-    /**
-     * 元素出栈
-     *
-     * @return 出栈的元素
-     */
-    public Item pop() {
-        // 获取头节点的元素，并更新头节点
-        Item item = first.item;
-        first = first.next;
-        // 更新节点的数量
-        N--;
-
-        return item;
     }
 
     /**
@@ -83,6 +45,8 @@ public class Stack<Item> implements Iterable<Item> {
      *
      * @return 迭代对象
      */
+    @Override
+    @NotNull
     public Iterator<Item> iterator() {
         return new ListIterator();
     }
@@ -99,11 +63,9 @@ public class Stack<Item> implements Iterable<Item> {
          * @return {@code true} 有下一个元素
          *         {@code false} 没有下一个元素
          */
+        @Override
         public boolean hasNext() {
             return current != null;
-        }
-
-        public void remove() {
         }
 
         /**
@@ -111,6 +73,7 @@ public class Stack<Item> implements Iterable<Item> {
          *
          * @return {@code Item} 泛型类型的对象
          */
+        @Override
         public Item next() {
             Item item = current.item;
             current = current.next;
