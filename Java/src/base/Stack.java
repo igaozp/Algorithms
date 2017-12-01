5074869f799529b4b8208642c9ba6b91757b58c5
KuoPing Kao <igaozp@hotmail.com>
2017-12-01 20:31:54 +0800

    [Add] 添加 Stack 的测试程序，添加异常处理和补全函数

--- Java/src/base/Stack.java
+++ Java/src/base/Stack.java
@@ -1,8 +1,11 @@
 package base;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * Stack 链表的实现
@@ -10,32 +13,40 @@ import java.util.Iterator;
  *
  * @param <Item> 泛型类型
  * @author igaozp
- * @version 1.1
+ * @version 1.2
  * @since 2017-6-30
  */
 public class Stack<Item> implements Iterable<Item> {
     /**
      * 栈的顶部节点
      */
-    private Node first;
+    private Node<Item> first;
     /**
      * 栈的节点元素数量
      */
-    private int N;
+    private int size;
 
     /**
      * 内部的链表节点类
      */
-    private class Node {
-        Item item;
-        Node next;
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    /**
+     * 构造方法
+     */
+    public Stack() {
+        first = null;
+        size = 0;
     }
 
     /**
      * 检查栈是否为空
      *
      * @return {@code true} 栈为空
-     * {@code false} 栈不为空
+     *         {@code false} 栈不为空
      */
     public boolean isEmpty() {
         return first == null;
@@ -47,7 +58,7 @@ public class Stack<Item> implements Iterable<Item> {
      * @return 节点元素数量
      */
     public int size() {
-        return N;
+        return size;
     }
 
     /**
@@ -57,12 +68,12 @@ public class Stack<Item> implements Iterable<Item> {
      */
     public void push(Item item) {
         // 使用头插法将元素入栈
-        Node oldFirst = this.first;
-        this.first = new Node();
+        Node<Item> oldFirst = this.first;
+        this.first = new Node<>();
         this.first.item = item;
         this.first.next = oldFirst;
         // 更新节点数量
-        N++;
+        size++;
     }
 
     /**
@@ -71,13 +82,44 @@ public class Stack<Item> implements Iterable<Item> {
      * @return 出栈的元素
      */
     public Item pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack underflow");
+        }
+
         // 获取头节点的元素，并更新头节点
         Item item = first.item;
         first = first.next;
         // 更新节点的数量
-        N--;
+        size--;
 
         return item;
+    }
+
+    /**
+     * 获取栈的首节点的元素
+     *
+     * @return 首节点元素
+     */
+    public Item peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack underflow");
+        }
+        return first.item;
+    }
+
+    /**
+     * 重写 {@code toString()} 方法
+     *
+     * @return toString
+     */
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+        }
+        return s.toString();
     }
 
     /**
@@ -88,24 +130,41 @@ public class Stack<Item> implements Iterable<Item> {
     @NotNull
     @Override
     public Iterator<Item> iterator() {
-        return new ListIterator();
+        return new ListIterator<>(first);
     }
 
     /**
      * 自定义 ListIterator 用来实现 Iterable 接口
      */
-    private class ListIterator implements Iterator<Item> {
-        private Node current = first;
+    private class ListIterator<Item> implements Iterator<Item> {
+        private Node<Item> current;
+
+        /**
+         * 构造方法
+         *
+         * @param first 构造方法参数
+         */
+        public ListIterator(Node<Item> first) {
+            current = first;
+        }
 
         /**
          * 检查是否有下一个元素
          *
          * @return {@code true} 有下一个元素
-         * {@code false} 没有下一个元素
+         *         {@code false} 没有下一个元素
          */
         @Override
         public boolean hasNext() {
             return current != null;
+        }
+
+        /**
+         * remove 方法
+         */
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
         }
 
         /**
@@ -115,9 +174,31 @@ public class Stack<Item> implements Iterable<Item> {
          */
         @Override
         public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
             Item item = current.item;
             current = current.next;
             return item;
         }
+    }
+
+    /**
+     * Stack 的单元测试
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) {
+                stack.push(item);
+            } else if (!stack.isEmpty()) {
+                StdOut.print(stack.pop() + " ");
+            }
+        }
+        StdOut.println("(" + stack.size() + " left on stack)");
     }
 }
