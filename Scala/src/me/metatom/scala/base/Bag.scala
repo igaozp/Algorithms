a69014ee6a1fc0dd59c5789ec797db9f976c5028
Fruent Neon <igaozp@hotmail.com>
2019-01-20 19:59:53 +0800

    Add Bag Scala version

---
+++ Scala/src/me/metatom/scala/base/Bag.scala
@@ -0,0 +1,73 @@
+package me.metatom.scala.base
+
+import scala.reflect.Manifest
+
+/**
+  * Bag 的链表实现
+  * Bag 是一种不支持删除其中元素的集合数据类型，可以用来收集和遍历元素
+  *
+  * @author igaozp
+  * @since 2019-01-19
+  * @version 1.0
+  * @tparam Item 泛型参数
+  */
+class Bag[Item >: Null <: AnyRef](implicit manifest: Manifest[Item]) {
+  /**
+    * 链表首节点
+    */
+  private var first = new Node
+  /**
+    * Bag 的元素数量
+    */
+  private var size = 0
+
+  /**
+    * 内部节点类
+    */
+  class Node {
+    var item = manifest.erasure.newInstance.asInstanceOf[Item]
+    var next: Node = _
+  }
+
+  /**
+    * 检查是否为空
+    *
+    * @return true Bag 为空
+    *         false Bag 不为空
+    */
+  def isEmpty: Boolean = size <= 0
+
+  /**
+    * 获取 Bag 的元素数量
+    *
+    * @return Bag 的元素数量
+    */
+  def getSize: Int = size
+
+  /**
+    * 添加元素
+    *
+    * @param item 添加的元素对象
+    */
+  def add(item: Item): Unit = {
+    val oldFirst: Node = this.first
+    this.first = new Node
+    this.first.item = item
+    this.first.next = oldFirst
+    size += 1
+  }
+}
+
+object Main {
+  /**
+    * Bag 的单元测试
+    *
+    * @param args 命令行参数
+    */
+  def main(args: Array[String]): Unit = {
+    val bag = new Bag[String]
+    bag.add("Hello")
+    bag.add("World")
+    print("Size of bag = " + bag.getSize)
+  }
+}
