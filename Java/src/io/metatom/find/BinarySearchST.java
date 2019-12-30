4e3796542c5d7a885ae73f4b09266ceab21e04b2
igaozp <igaozp@hotmail.com>
2019-12-30 20:36:40 +0800

    优化 BinarySearchST.java

--- Java/src/io/metatom/find/BinarySearchST.java
+++ Java/src/io/metatom/find/BinarySearchST.java
@@ -1,9 +1,9 @@
 package io.metatom.find;
 
-import edu.princeton.cs.algs4.StdIn;
 import io.metatom.base.Queue;
 
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
 /**
  * 二分查找（基于有序数组）
@@ -29,7 +29,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
     /**
      * 存储值的数组
      */
-    private Value[] vals;
+    private Value[] values;
     /**
      * 查找表的长度
      */
@@ -49,7 +49,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
      */
     public BinarySearchST(int capacity) {
         keys = (Key[]) new Comparable[capacity];
-        vals = (Value[]) new Object[capacity];
+        values = (Value[]) new Object[capacity];
     }
 
     /**
@@ -59,15 +59,15 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
      */
     private void resize(int capacity) {
         assert capacity >= size;
-        Key[] tempOfKey = (Key[]) new Comparable[capacity];
-        Value[] tempOfValue = (Value[]) new Object[capacity];
+        var tempOfKeys = (Key[]) new Comparable[capacity];
+        var tempOfValues = (Value[]) new Object[capacity];
 
         for (int i = 0; i < size; i++) {
-            tempOfKey[i] = keys[i];
-            tempOfValue[i] = vals[i];
+            tempOfKeys[i] = keys[i];
+            tempOfValues[i] = values[i];
         }
-        vals = tempOfValue;
-        keys = tempOfKey;
+        values = tempOfValues;
+        keys = tempOfKeys;
     }
 
     /**
@@ -82,8 +82,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
     /**
      * 检查查找表是否为空
      *
-     * @return {@code true} 表为空
-     * {@code false} 表不为空
+     * @return {@code true} 表为空 {@code false} 表不为空
      */
     public boolean isEmpty() {
         return size == 0;
@@ -99,14 +98,13 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         if (key == null) {
             throw new IllegalArgumentException("argument to get() is null");
         }
-
         if (isEmpty()) {
             return null;
         }
         // 查找到的键的坐标
         int i = rank(key);
         if (i < size && keys[i].compareTo(key) == 0) {
-            return vals[i];
+            return values[i];
         }
         return null;
     }
@@ -121,24 +119,23 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         if (key == null) {
             throw new IllegalArgumentException("argument to rank() is null");
         }
-
-        int lo = 0;
-        int hi = size - 1;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
+        int low = 0;
+        int high = size - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
             int cmp = key.compareTo(keys[mid]);
             if (cmp < 0) {
                 // 查找的键小于中间键
-                hi = mid - 1;
+                high = mid - 1;
             } else if (cmp > 0) {
                 // 查找的键大于中间键
-                lo = mid + 1;
+                low = mid + 1;
             } else {
                 // 返回找到的相应的值
                 return mid;
             }
         }
-        return lo;
+        return low;
     }
 
     /**
@@ -151,25 +148,22 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         if (key == null) {
             throw new IllegalArgumentException("first argument to put() is null");
         }
-
         int i = rank(key);
         if (i < size && keys[i].compareTo(key) == 0) {
-            vals[i] = val;
+            values[i] = val;
             return;
         }
-
         // 扩容
         if (size == keys.length) {
             resize(2 * keys.length);
         }
-
         // 插入新的元素
         for (int j = size; j > i; j--) {
             keys[j] = keys[j - 1];
-            vals[j] = vals[j - 1];
+            values[j] = values[j - 1];
         }
         keys[i] = key;
-        vals[i] = val;
+        values[i] = val;
         size++;
     }
 
@@ -186,12 +180,11 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         if (isEmpty()) {
             return;
         }
-
         // 删除元素
         int i = rank(key);
         for (int j = i; j < size - 1; j++) {
             keys[j] = keys[j + 1];
-            vals[j] = vals[j + 1];
+            values[j] = values[j + 1];
         }
         size--;
     }
@@ -263,7 +256,6 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
         if (key == null) {
             throw new IllegalArgumentException("argument to ceiling() is null");
         }
-
         int i = rank(key);
         if (i == size) {
             return null;
@@ -305,44 +297,40 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
      *
      * @return 包含所有键的队列
      */
-    public Iterable<Key> keys(Key lo, Key hi) {
-        if (lo == null) {
+    public Iterable<Key> keys(Key low, Key high) {
+        if (low == null) {
             throw new IllegalArgumentException("first argument to keys() is null");
         }
-        if (hi == null) {
+        if (high == null) {
             throw new IllegalArgumentException("second argument to keys() is null");
         }
-
-        Queue<Key> q = new Queue<>();
-        for (int i = rank(lo); i < rank(hi); i++) {
-            q.enqueue(keys[i]);
+        var queue = new Queue<Key>();
+        for (int i = rank(low); i < rank(high); i++) {
+            queue.enqueue(keys[i]);
         }
-        if (contains(hi)) {
-            q.enqueue(keys[rank(hi)]);
+        if (contains(high)) {
+            queue.enqueue(keys[rank(high)]);
         }
-        return q;
+        return queue;
     }
 
     /**
      * 检查是否存在给定键的节点
      *
      * @param key 需要查找的键
-     * @return {@code true} 节点存在
-     * {@code false} 节点不存在
+     * @return {@code true} 节点存在 {@code false} 节点不存在
      */
     public boolean contains(Key key) {
         if (key == null) {
             throw new IllegalArgumentException("argument to contains() is null");
         }
-
         return get(key) != null;
     }
 
     /**
      * 正确性检测
      *
-     * @return {@code true} 通过
-     * {@code false} 未通过
+     * @return {@code true} 通过 {@code false} 未通过
      */
     private boolean check() {
         return isSorted() && rankCheck();
@@ -351,11 +339,10 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
     /**
      * 检查是否已经排序
      *
-     * @return {@code true} 有序
-     * {@code false} 无序
+     * @return {@code true} 有序 {@code false} 无序
      */
     private boolean isSorted() {
-        for (int i = 0; i < size; i++) {
+        for (int i = 1; i < size; i++) {
             if (keys[i].compareTo(keys[i - 1]) < 0) {
                 return false;
             }
@@ -366,8 +353,7 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
     /**
      * 随机检查
      *
-     * @return {@code true} 有序
-     * {@code false} 无序
+     * @return {@code true} 有序 {@code false} 无序
      */
     private boolean rankCheck() {
         for (int i = 0; i < size; i++) {
@@ -389,14 +375,14 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> {
      * @param args 命令行参数
      */
     public static void main(String[] args) {
-        BinarySearchST<String, Integer> st = new BinarySearchST<>(5);
-
+        var st = new BinarySearchST<String, Integer>(5);
+        var scanner = new Scanner(System.in);
         for (int i = 0; i < 5; i++) {
-            String key = StdIn.readString();
+            var key = scanner.next();
             st.put(key, i);
         }
 
-        for (String s : st.keys()) {
+        for (var s : st.keys()) {
             System.out.println(s + " " + st.get(s));
         }
     }
